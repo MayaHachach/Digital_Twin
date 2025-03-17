dd97a97009b137615e67f03c8b4df1570f9cf8ca
Maya Hashash <mayahashash00@gmail.com>
2025-03-17 12:40:02 +0200

    added husky_odom_alignment node to the launch file

--- src/communication/launch/communication.launch.py
+++ src/communication/launch/communication.launch.py
@@ -24,12 +24,25 @@ def generate_launch_description():
         name='request_STOD_server',
         output='screen'
     )
+    
+    husky_odom_alignment = Node(
+        package='communication',
+        executable='odom_alignment',
+        name='odom_alignment',
+        output='screen'
+    )
 
     # Create and return launch description
     ld = LaunchDescription()
 
     ld.add_action(communicator_node)
+    
     ld.add_action(request_history_server)
+    
+    #! run this node separately if you think you're gonna be requesting the STOD more than once from omniverse.
     ld.add_action(request_STOD_server)
+    
+    #! comment this if you're not using the husky.
+    ld.add_action(husky_odom_alignment)
 
     return ld
